e8491688357fc3baa5fc081a832cb8dc5f25f43a
izman1234 <cnchi9999@gmail.com>
2023-10-21 14:36:27 -0400

    added Isaac's code

--- mcu/MilestoneDemo1/MilestoneDemo1.ino
+++ mcu/MilestoneDemo1/MilestoneDemo1.ino
@@ -1,24 +1,35 @@
 #include <ArduinoBLE.h>
-//#include <Arduino_FreeRTOS.h>
-//#include <semphr.h>
+#include <capacitive_sensor.h>
+#include <Servo.h>
+#include <mbed.h>
 
 BLEService IdentificationService("1234");
 BLEIntCharacteristic getIdentification("00002a05-0000-1000-8000-00805f9b34fb", BLERead | BLEWrite);
 
-//void AdvertiseTask( void *pvParameters);
-
 const int ledPin = LED_BUILTIN;
 uint16_t value;
 
+Servo myservo;
+
+CapacitiveSensor cs_4_3 = CapacitiveSensor(4,3);
+
 void setup() {
 
+  cs_4_3.set_CS_AutocaL_Millis(0xFFFFFFFF);
   Serial.begin(9600);
+
+  myservo.attach(2);
+  delay(1000);
+  myservo.write(0);
+  delay(100);
+  myservo.detach();
   
   if (!BLE.begin()) {
     Serial.println("starting Bluetooth® Low Energy module failed!");
     while (1);
   }
   pinMode(LED_BUILTIN, OUTPUT);
+
 
   BLE.setLocalName("Smart Handle");
 
@@ -27,6 +38,23 @@ void setup() {
 
   getIdentification.setValue(0);
 
+}
+
+void loop() {
+  BLEDevice peripheral = BLE.available();
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  while(!BLE.connected()) {
+    AdvertiseInterval();
+  }
+
+  if (BLE.connected()) {
+     digitalWrite(LED_BUILTIN, HIGH);
+     Serial.println("Connected");
+
+     readValue();
+  }
 }
 
 void AdvertiseInterval() {
@@ -42,6 +70,8 @@ void readValue() {
     getIdentification.readValue(value);
     Serial.println(value);
     if(value == 52) {
+      cap();
+      turnServo();
       BLE.disconnect();
       Serial.println("Disconnected");
       value = 0;
@@ -50,18 +80,20 @@ void readValue() {
   }
 }
 
-void loop() {
-  BLEDevice peripheral = BLE.available();
+void cap() {
+  while(cs_4_3.capacitiveSensor(30) < 6000);
+}
 
-  digitalWrite(LED_BUILTIN, LOW);
-
-  while(!BLE.connected()) {
-    AdvertiseInterval();
-  }
-  if (BLE.connected()) {
-     digitalWrite(LED_BUILTIN, HIGH);
-     Serial.println("Connected");
-
-     readValue();
-  }
+void turnServo() {
+  myservo.attach(2);
+  delay(1000);
+  myservo.write(180);
+  delay(100);
+  myservo.detach();
+  delay(1000);
+  myservo.attach(2);
+  delay(1000);
+  myservo.write(0);
+  delay(100);
+  myservo.detach();
 }
