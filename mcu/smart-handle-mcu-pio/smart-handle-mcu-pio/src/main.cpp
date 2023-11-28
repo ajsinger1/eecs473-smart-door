97ac2cac7633a0ed262afccd60a111506979b05b
Neel Dutta <dutta.k.neel@gmail.com>
2023-11-28 01:02:12 -0500

    using dedicated hardware for pwm

--- mcu/smart-handle-mcu-pio/smart-handle-mcu-pio/src/main.cpp
+++ mcu/smart-handle-mcu-pio/smart-handle-mcu-pio/src/main.cpp
@@ -44,13 +44,18 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
   deadbolt.init();
-  aws_init();
-  ble_init();
-  touchAttachInterrupt(TOUCH_PIN, handle_touch_isr, TOUCH_THRESHOLD);
-  xTaskCreatePinnedToCore(cap_unlock_handler, "t1", 16384, NULL, configMAX_PRIORITIES - 1, NULL, 1);
-  Serial.println("Finish setup");
-  vTaskDelete(NULL);
+  // aws_init();
+  // ble_init();
+  // touchAttachInterrupt(TOUCH_PIN, handle_touch_isr, TOUCH_THRESHOLD);
+  // xTaskCreatePinnedToCore(cap_unlock_handler, "t1", 16384, NULL, configMAX_PRIORITIES - 1, NULL, 1);
+  // Serial.println("Finish setup");
+  // vTaskDelete(NULL);
   // xTaskCreatePinnedToCore(ble_disconnect_task, "ble_disconnect_task", 5120, NULL, 1, NULL, 1);
+
+ 
+  
+
+
 }
 
 void loop() {
@@ -58,4 +63,8 @@ void loop() {
   // vTaskDelay(1000 / portTICK_PERIOD_MS);
   // // Serial.println("In ISR? " + String(xPortInIsrContext()));
   // state.isUnlockUserConnected();
+  deadbolt.unlock();
+  
+  deadbolt.lock();
+
 }
